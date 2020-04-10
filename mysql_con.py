f51eb69e0709f24590b8860c54ef7b669f32d7ef
Rafael Araújo <rafarvns@gmail.com>
2020-04-10 13:43:07 -0300

    Update mysql_con.py

--- mysql_con.py
+++ mysql_con.py
@@ -7,37 +7,37 @@ try:
                                          user='root',
                                          password='123456')
     create_tabelas_e_adiciona_dados_sql = """    
-        CREATE TABLE IF NOT EXISTS `dificuldade` (
-            `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            `nivel` VARCHAR(20)
+        CREATE TABLE IF NOT EXISTS dificuldade (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            nivel VARCHAR(20)
         );
         
-        CREATE TABLE IF NOT EXISTS `soletrando` (
-            `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            `palavra` VARCHAR(100),
-            `palavra_comp` VARCHAR(140),
-            `dificuldade` INT,
+        CREATE TABLE IF NOT EXISTS soletrando (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            palavra VARCHAR(100),
+            palavra_comp VARCHAR(140),
+            dificuldade INT,
             CONSTRAINT fk_dificuldade
             FOREIGN KEY (dificuldade) 
                 REFERENCES dificuldade(id)
         );
 
-        INSERT INTO `dificuldade` (nivel) VALUES ('Fácil');
-        INSERT INTO `dificuldade` (nivel) VALUES ('Normal');
-        INSERT INTO `dificuldade` (nivel) VALUES ('Difícil');
+        INSERT INTO dificuldade (nivel) VALUES ('Fácil');
+        INSERT INTO dificuldade (nivel) VALUES ('Normal');
+        INSERT INTO dificuldade (nivel) VALUES ('Difícil');
 
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('CASA', 'c. a. s. a.', 1);
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('ROUPA', 'r. o. u. p. a.', 1);
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('OLHOS', 'o. l. h. o. s.', 1);
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('SOPA', 's. o. p. a.', 1);
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('UVA', 'u. v. a.', 1);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('CASA', 'c. a. s. a.', 1);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('ROUPA', 'r. o. u. p. a.', 1);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('OLHOS', 'o. l. h. o. s.', 1);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('SOPA', 's. o. p. a.', 1);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('UVA', 'u. v. a.', 1);
 
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('PANELA', 'p. a. n. e. l. a.', 2);
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('TECLADO', 't. e. c. l. a. d. o.', 2);
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('MONITOR', 'm. o. n. i. t. o. r.', 2);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('PANELA', 'p. a. n. e. l. a.', 2);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('TECLADO', 't. e. c. l. a. d. o.', 2);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('MONITOR', 'm. o. n. i. t. o. r.', 2);
 
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('ROTEADOR', 'r. o. t. e. a. d. o. r.', 3);
-        INSERT INTO `soletrando` (palavra, palavra_comp, dificuldade) VALUES ('AQUECEDOR', 'a. q. u. e. c. e. d. o. r.', 3);        
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('ROTEADOR', 'r. o. t. e. a. d. o. r.', 3);
+        INSERT INTO soletrando (palavra, palavra_comp, dificuldade) VALUES ('AQUECEDOR', 'a. q. u. e. c. e. d. o. r.', 3);       
      """
 
     if connection.is_connected():
