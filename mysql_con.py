78ebdb7630438b071125c3969bb5101f0132c7f5
Rafael Araújo <rafarvns@gmail.com>
2020-04-10 13:47:20 -0300

    Update mysql_con.py

--- mysql_con.py
+++ mysql_con.py
@@ -45,9 +45,7 @@ try:
         print("Connected to MySQL Server version ", db_Info)
         cursor = connection.cursor()
         result = cursor.execute(create_tabelas_e_adiciona_dados_sql)  
-        cursor.fetchall()  
         result_d = cursor.execute(add_data_sql) 
-        cursor.fetchall() 
         connection.commit()
         print("Tabelas criadas e Dados adicionados com sucesso!")
 
